103060a23df99e23db035011f99c445789e8baa2
aalnoman <abdullahalnoman2042@gmail.com>
2023-03-28 19:21:22 +0600

    cart

--- ema-jhon-by-react/src/App.jsx
+++ ema-jhon-by-react/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './components/Header/Header'
-import Shop from './components/Header/shop/Shop'
+import Shop from './components/shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
--- ema-jhon-by-react/src/components/shop/Shop.jsx
+++ ema-jhon-by-react/src/components/shop/Shop.jsx
@@ -1,25 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import Product from '../../product/Product';
+import Product from '../product/Product';
 import './Shop.css'
 
 const Shop = () => {
 
     const[products, setProducts]=  useState([]);
 
+    const [cart , setCart] = useState([])
+
  useEffect(()=>{
-    fetch('../../../../public/fakeData/products.json')
+    fetch('../../../../fakeData/products.json')
     .then(res => res.json())
     .then(data => setProducts(data))
  },[])
+
+ const handleAddToCart = (product)=>{
+            const newCart = [...cart, product ]
+            setCart(newCart)
+}
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product key={product.id} product={product}
+                    handleAddToCart={handleAddToCart}
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h2>order</h2>
+                <p>selected item: {cart.length}</p>
             </div>
         </div>
     );
