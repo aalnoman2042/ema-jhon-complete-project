9528c9c0409347202e1edc3ea7b0d12f0a2b558e
aalnoman <abdullahalnoman2042@gmail.com>
2023-04-06 23:36:25 +0600

    reviwe item

--- ema-jhon-by-react/src/components/orders/Orders.jsx
+++ ema-jhon-by-react/src/components/orders/Orders.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import Product from '../product/Product';
+import ReviewItem from '../reviewItem/ReviewItem';
+import "./Order.css"
+import { removeFromDb } from '../../../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData()
-    console.log(cart);
+    const savedCart = useLoaderData()
+    const [cart, setCart ] = useState(savedCart)
+
+    const handelRemoveFromCart = (id) =>{
+       const remaining = cart.filter(product => product.id !== id )
+       setCart(remaining)
+       removeFromDb(id)
+    }
+
+    console.log(savedCart);
 
     return (
         <div className='shop-container'>
-           <div className='products-container'>
-           <h2>products lenght:{cart.length}</h2>
+           <div className='review-container'>
+           {
+            cart.map(product => <ReviewItem 
+                key={product.id}
+                 product={product}
+                 handelRemoveFromCart={handelRemoveFromCart}
+            ></ReviewItem>)
+           }
            </div>
            <div className='cart-container'>
             <Cart cart={cart}></Cart>
---
+++ ema-jhon-by-react/src/components/reviewItem/ReviewItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./ReviewItem.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+
+
+const ReviewItem = ({product , handelRemoveFromCart}) => {
+    const {id, img , name , quantity, price} = product
+    console.log(img);
+    return (
+        <div className='review-item'>
+          <img src={img} alt="" /> 
+          <div className='review-info'>
+            <p className='product-title'>{name}</p>
+            <p>price: <span className='orange-text'>${price}</span></p>
+            <p>order quantity: <span className='orange-text'>{quantity}</span></p>
+          </div>
+          <button onClick={()=>handelRemoveFromCart(id)} className='btn-delete'><FontAwesomeIcon icon={faTrashCan} /></button>
+        </div>
+    );
+};
+
+export default ReviewItem;
